0be255af38c4bcaff49300f4c1aea60de8f34823
Victor Nilsson <victor.nilsson@etraveligroup.com>
2024-09-02 13:50:39 +0200

    Add markview keymaps

--- config/utils/markview.nix
+++ config/utils/markview.nix
@@ -2,4 +2,14 @@
   plugins.markview = {
     enable = true;
   };
+  keymaps = [
+    {
+      mode = "n";
+      key = "<leader>cp";
+      action = "<cmd>Markview<cr>";
+      options = {
+        desc = "Markdown Preview";
+      };
+    }
+  ];
 }
