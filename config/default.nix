445ef670e8c903333604a23cd26255045aeaa463
Victor Nilsson <victor.nilsson@etraveligroup.com>
2024-09-08 21:33:46 +0200

    remove hardtime

--- config/default.nix
+++ config/default.nix
@@ -55,7 +55,7 @@
 
     ./utils/better-escape.nix
     # ./utils/neocord.nix
-    ./utils/hardtime.nix
+    # ./utils/hardtime.nix
     ./utils/harpoon.nix
     ./utils/illuminate.nix
     # ./utils/markdown-preview.nix
