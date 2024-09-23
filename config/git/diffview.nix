a7e8770764c1a6a7600c6e3cb590c3869b65581f
Victor Nilsson <victor.nilsson@etraveligroup.com>
2024-09-23 09:46:04 +0200

    Add diffview keymaps

--- config/git/diffview.nix
+++ config/git/diffview.nix
@@ -2,4 +2,24 @@
   plugins.diffview = {
     enable = true;
   };
+  keymaps = [
+    {
+      mode = ["n" "v"];
+      key = "<leader>gdo";
+      action = "DiffViewOpen";
+      options = {
+        silent = true;
+        desc = "+diffview open";
+      };
+    }
+    {
+      mode = ["n" "v"];
+      key = "<leader>gdf";
+      action = "DiffViewFileHistory";
+      options = {
+        silent = true;
+        desc = "+diffview File History";
+      };
+    }
+  ];
 }
